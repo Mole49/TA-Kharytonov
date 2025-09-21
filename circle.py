d7ce299642e105d3252d6274b78b2d82bb8eeaae
Mole49 <denisharitonov776@gmail.com>
2025-09-21 20:54:30 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,6 +1,5 @@
 import math
-import msvcrt  # Windows only
-
+import msvcrt  
 R = float(input("Enter circle radius R: "))
 L = 2 * math.pi * R
 S = math.pi * R**2
@@ -10,3 +9,4 @@ print(f"Area of circle: {S:.2f}")
 
 print("\nPress any key to exit...")
 msvcrt.getch()
+
