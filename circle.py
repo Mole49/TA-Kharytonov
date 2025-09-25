f1fd1a79ffc2fa47d984ab55562c0d4163e480e8
Mole49 <denisharitonov776@gmail.com>
2025-09-25 12:36:47 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,11 +1,16 @@
 import math
-import msvcrt  
-R = float(input("Enter circle radius R: "))
-L = 2 * math.pi * R
-S = math.pi * R**2
+import msvcrt
 
-print(f"Length of circle: {L:.2f}")
-print(f"Area of circle: {S:.2f}")
+R = float(input("Enter circle radius R: "))
+
+if R < 0:
+    print("R не може бути від'ємним")
+else:
+    L = 2 * math.pi * R
+    S = math.pi * R**2
+    print(f"Length of circle: {L:.2f}")
+    print(f"Area of circle: {S:.2f}")
 
 print("\nPress any key to exit...")
 msvcrt.getch()
+
